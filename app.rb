31036366eb6cc104a77e647cf29c8c757fd8779a
keGZ <kegz@mail.ru>
2017-11-16 17:04:13 +0800

    l27 def before

--- app.rb
+++ app.rb
@@ -2,6 +2,31 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def get_db
+	@db = SQLIte3::Database.new (Dir.pwd + '/db/leprozorium.db')
+	@db.redults_as_hash
+end
+
+before do
+
+end
+
+configure do
+
+end
+
+
+get '/new' do
+	erb :new
+end
+
+post '/new' do
+  erb "Запись добавлена"
+end
+
+
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
