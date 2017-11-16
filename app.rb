de134ac4aa275e4a7792ef0382cb1989dbc688a2
keGZ <kegz@mail.ru>
2017-11-16 20:41:57 +0800

    l28 блог

--- app.rb
+++ app.rb
@@ -3,31 +3,62 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'date'
+
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
 
 def get_db
-	@db = SQLite3::Database.new (Dir.pwd + '/db/leprozorium.db')
-	@db.results_as_hash
+  @db = SQLite3::Database.new (Dir.pwd + '/db/leprozorium.db')
+  @db.results_as_hash = true
 end
 
-before do
-  get_db
-end
 
 configure do
-
+  get_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS Posts
+  (
+  "Id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+  "date_create" DATE,
+  "content" TEXT
+  )'
 end
 
 
 get '/new' do
-	erb :new
+  erb :new
 end
 
 post '/new' do
-  erb "Запись добавлена"
-end
+  @text_new_post = params[:text_new_post]
+  @date_new_post = Date.today
+  if @text_new_post.length <= 0
+    @error = 'Введите текст'
+    return erb :new
+  end
+    get_db
+    @db.execute 'INSERT INTO
+  "Posts"
+  (
+      "date_create",
+      "content"
+  )
+  VALUES
+  (
+  datetime(), ?
+  )', [@text_new_post]
+    erb "Запись добавлена"
+  end
 
 
-
-get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
-end
+  get '/' do
+    get_db
+    @results = @db.execute 'select * from Posts order by id desc'
+    erb :index
+  end
---
+++ test.rb
@@ -0,0 +1,4 @@
+require 'date'
+date_new_post = Date.today
+
+p date_new_post[:date]
